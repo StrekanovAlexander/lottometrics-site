42db13aa05778dd8a9e61e2d925c8dd19695d3c9
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-11-26 23:00:07 +0100

    added: Google Analytics code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./globals.css";
@@ -14,6 +15,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8MCJSXD9GG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8MCJSXD9GG');
+          `}
+        </Script>
+      </head>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow max-w-5xl mx-auto p-8">
