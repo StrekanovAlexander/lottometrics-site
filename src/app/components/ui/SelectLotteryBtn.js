a8c17f11b511e973c376823c59491e4d7db11872
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-12-02 23:25:13 +0100

    rebuilded: analisis page

--- src/app/components/ui/SelectLotteryBtn.js
+++ src/app/components/ui/SelectLotteryBtn.js
@@ -14,8 +14,8 @@ export default function SelectLotteryBtn() {
         <button
             onClick={() => setOpen(!open)}
             type="button"
-            className="inline-flex items-center justify-center rounded-md px-4 py-2 
-              bg-gray-800 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none"
+            className="inline-flex items-center justify-center border border-gray-300 rounded-md px-3 py-2 
+               shadow-md bg-white text-gray-800 text-sm font-medium hover:bg-gray-50  focus:outline-none"
         >
             <ChevronDown className="w-5 h-5 mr-2" />
             Change Lottery
