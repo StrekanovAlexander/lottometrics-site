754f7583b6255d2dbbf5a737a349b48224ea0a19
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-12-07 14:20:02 +0100

    fixed: LotteryData

--- src/app/components/lotteries/LotteriesData.js
+++ src/app/components/lotteries/LotteriesData.js
@@ -33,7 +33,8 @@ export default function LotteriesData() {
             { loading && <Spinner />} 
             <div className="
                 grid 
-                gap-6 
+                gap-2
+                md:gap-6 
                 grid-cols-1 
                 sm:grid-cols-2 
                 md:grid-cols-3 
