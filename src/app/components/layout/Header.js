6ce8fcbcd61a9afe5f6187bfe4dbbdfa1c8a44c2
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-12-15 19:44:35 +0100

    rebuilded: header, logo, etc

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 p-4 bg-graphite-dark shadow-sm flex justify-between items-center z-50">
       <a href="/" className="text-yellow font-semibold flex items-center gap-2">
-        <ChartPie size={20} />
+        <img src="/images/logo/Lottometrics_Logo_128.png" class="h-4 md:h-4 lg:h-6 w-auto" />
         LottoMetrics
       </a>
       {/* Desktop nav */}
