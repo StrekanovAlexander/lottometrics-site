c6a18d0e04a0783cfda65e82f5a3486940cf042b
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-12-21 23:18:34 +0100

    rebuilede: RhythmCard

--- src/app/components/elements/cards/RhythmCard.js
+++ src/app/components/elements/cards/RhythmCard.js
@@ -3,12 +3,11 @@ import { useDashboard } from "@/context/DashboardContext";
 export default function RhythmCard({item}) {
     const { selectedNumber, setSelectedNumber } = useDashboard();
     const bgClass = item.draw_number === selectedNumber ? 'bg-heatmap-active' : 'bg-gray-300';
-    const borderClass = item.draw_number === selectedNumber ? 'border-heatmap-active' : 'border-gray-300';
+    const borderClass = item.draw_number === selectedNumber ? 'border-heatmap-active' : 'border-gray-400';
     return (
         <div
             onClick={() => setSelectedNumber(item.draw_number)} 
-            className={`bg-white border border-1 ${borderClass} rounded-sm text-gray-900 
-            pt-1 px-1 flex flex-col items-center cursor-pointer`}
+            className={`bg-white text-gray-900 pt-1 px-1 flex flex-col items-center cursor-pointer`}
         >
             <span
                 onClick={() => setSelectedNumber(item.draw_number)} 
@@ -17,7 +16,9 @@ export default function RhythmCard({item}) {
             >
                 {item.draw_number}
             </span>
-            <div className={`text-xxs flex flex-col gap-1 text-center font-semibold`}>
+            <div className={`text-xxs text-center font-semibold 
+                border border-1 ${borderClass} rounded-md px-1 my-2`}
+            >
                 {item.gaps.join('-')}
             </div>
         </div>    
